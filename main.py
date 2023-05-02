2f92d31ec2174b65f913256eebe93b8560aca9fa
alina.polezhaeva <alpo02@dfki.de>
2023-05-02 17:13:14 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas
+
+
+def print_hi():
+    file = pandas.read_csv("C:/Users/Admin/Desktop/Studium/BDA/publ_titles/publTitles.csv")
+
+
+
+if __name__ == '__main__':
+    print_hi()
+
+
